418289bae007fb75468dfbed91974f75c19ae402
garateHegoi <garate.hegoi@uni.eus>
2022-11-21 13:40:47 +0100

    bukatuta

---
+++ Ariketa1/Ariketa1/Controllers/kaixomunduaController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Web;
+
+namespace Ariketa1.Controllers
+{
+    public class kaixomunduaController : Controller
+    {
+        private string izena;
+        private string zenbat;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult OngiEtorri(string izena, int zenbat = 1)
+        {
+            ViewBag.Mezua = "Hello " + izena;
+            ViewBag.Zenbat = zenbat;
+            return View();
+        }
+    }
+}
